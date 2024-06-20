756f2f0152a21cb0dae8651405fe52e6aab49f37
zhizhikkkk <zhanelmamyt@gmail.com>
2024-06-20 18:39:05 +0600

    finish 1

---
+++ GameStore/Migrations/20240620122227_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GameStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TITLE = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    CODE = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    VALUE = table.Column<decimal>(type: "numeric", nullable: false),
+                    A_DATE = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Currencies_A_DATE_CODE",
+                table: "Currencies",
+                columns: new[] { "A_DATE", "CODE" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
